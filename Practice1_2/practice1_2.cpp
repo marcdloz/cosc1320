134e289e30c1ed6abbb2ce9240cc973174347a8f
Marcos Lozano <marcdloz@utexas.edu>
2019-09-27 02:08:31 -0600

    Merge with remote repo

---
+++ Practice1_2/practice1_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>;
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World" << endl;
+	cout << "Enter two numbers to multiply:" << endl;
+	int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The product of the two numbers are " << v1 * v2 << endl;
+
+};
